bb1a28e963ef7911499097ad77e437731d3eeab8
Edgar Henriquez <edgaromar90@gmail.com>
2017-08-22 15:25:14 -0400

    small changes to css

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import Contact from './Contact'
 
 class ListContacts extends Component {
-    render() {
+  render() {
 
-        const { contacts } = this.props;
+    const { contacts } = this.props;
 
-        return(
-            <div className="list-contacts-wrapper">
-                <ul className="list-contacts row justify-content-center">
-                    { contacts.map(contact => (
-                        <li key={contact.id} className="col-11 col-sm-10 col-lg-8">
-                            <Contact
-                                contact={contact}
-                                onDeleteContact={this.props.onDeleteContact}/>
-                        </li>
-                        )
-                    )}
-                </ul>
-            </div>
-        );
-    }
+    return(
+      <div className="list-contacts-wrapper">
+        <ul className="list-contacts row justify-content-center">
+          { contacts.map(contact => (
+              <li key={contact.id} className="col-12 col-sm-10 col-lg-8">
+                <Contact
+                  contact={contact}
+                  onDeleteContact={this.props.onDeleteContact}/>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ListContacts;
