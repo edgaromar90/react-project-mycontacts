2769d2fa28295f5ed999bbfad9e3db05001408c4
Edgar Henriquez <edgaromar90@gmail.com>
2017-08-22 15:31:15 -0400

    reduced indentation from 4 to 2 spaces

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -3,48 +3,48 @@ import { Link } from 'react-router-dom';
 
 class CreateContact extends Component {
 
-    validateFullName = (input) => {
+  validateFullName = (input) => {
 
-    }
+  }
 
-    validateOccupation = (input) => {
+  validateOccupation = (input) => {
 
-    }
+  }
 
-    handleSubmit = (form) =>{
+  handleSubmit = (form) =>{
 
-    }
+  }
 
-    render() {
-        return(
-            <div>
-                <div className="row create-contact-top">
-                    <span className="input-group-addon" style={ {width:'69px', height:'60px', border:'none'} }>
-                        <Link to="/">
-                            <i className="fa fa-arrow-left text-primary" style={ {fontSize:'29px', paddingLeft: '10px'} } aria-hidden="true"></i>
-                        </Link>
-                    </span>
-                </div>
-                <div className="row create-contact-form-wrapper justify-content-center">
-                    <form className="col-11 col-lg-8 col-xl-5">
-                      <div className="form-group">
-                        <input type="text" className="form-control form-control-lg" placeholder="Enter Full Name" />
-                      </div>
-                      <div className="form-group">
-                        <input type="text" className="form-control form-control-lg" placeholder="Enter Occupation" />
-                      </div>
-                      <div className="form-group input-group">
-                        <input type="text" className="form-control form-control-lg" placeholder="Add Skills" aria-describedby="plusBtn" />
-                        <span className="input-group-addon">
-                            <i className="fa fa-plus text-primary" id="plusBtn" aria-hidden="true"></i>
-                        </span>
-                      </div>
-                      <button type="submit" className="btn btn-lg btn-primary float-right">Submit</button>
-                    </form>
-                </div>
+  render() {
+    return(
+      <div>
+        <div className="row create-contact-top">
+          <span className="input-group-addon" style={ {width:'69px', height:'60px', border:'none'} }>
+            <Link to="/">
+              <i className="fa fa-arrow-left text-primary" style={ {fontSize:'29px', paddingLeft: '10px'} } aria-hidden="true"></i>
+            </Link>
+          </span>
+        </div>
+        <div className="row create-contact-form-wrapper justify-content-center">
+          <form className="col-11 col-lg-8 col-xl-5">
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" placeholder="Enter Full Name" />
             </div>
-        );
-    }
+            <div className="form-group">
+              <input type="text" className="form-control form-control-lg" placeholder="Enter Occupation" />
+            </div>
+            <div className="form-group input-group">
+              <input type="text" className="form-control form-control-lg" placeholder="Add Skills" aria-describedby="plusBtn" />
+              <span className="input-group-addon">
+                <i className="fa fa-plus text-primary" id="plusBtn" aria-hidden="true"></i>
+              </span>
+            </div>
+            <button type="submit" className="btn btn-lg btn-primary float-right">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CreateContact;
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom';
 
 class Search extends Component {
 
-    state={
-        query: ''
-    }
+  state={
+    query: ''
+  }
 
-    handleChange = (query) =>{
-        this.setState({query});
-    }
+  handleChange = (query) =>{
+    this.setState({query});
+  }
 
-    render() {
-        return(
-            <div className="search row">
-                <div className="col-12 justify-content-center input-group input-group-lg">
-                    <input
-                        className="form-control col-10 col-lg-6"
-                        type="text"
-                        placeholder="Search Contact"
-                        value={this.state.query}
-                        onChange={(e) => this.handleChange(e.target.value)}
-                        aria-describedby="plusBtn" />
-                    <span className="input-group-addon" id="plusBtn">
-                        <Link to="/create/contact">
-                            <i className="fa fa-user-plus text-primary" aria-hidden="true"></i>
-                        </Link>
-                    </span>
-                </div>
-                <div className="col-12 justify-content-md-center">
-                    <div className="text-center">{this.state.query}</div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div className="search row">
+        <div className="col-12 justify-content-center input-group input-group-lg">
+          <input
+            className="form-control col-10 col-lg-6"
+            type="text"
+            placeholder="Search Contact"
+            value={this.state.query}
+            onChange={(e) => this.handleChange(e.target.value)}
+            aria-describedby="plusBtn" />
+          <span className="input-group-addon" id="plusBtn">
+            <Link to="/create/contact">
+              <i className="fa fa-user-plus text-primary" aria-hidden="true"></i>
+            </Link>
+          </span>
+        </div>
+        <div className="col-12 justify-content-md-center">
+          <div className="text-center">{this.state.query}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
