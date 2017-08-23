387664c0ff6eefae750f1eb1fdbab5b50b20c513
Edgar Henriquez <edgaromar90@gmail.com>
2017-08-23 19:54:58 -0400

    improving contact search

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import escapeRegExp from 'escape-string-regexp';
-import sortBy from 'sort-by';
 import Contact from './Contact';
+import escapeRegExp from 'escape-string-regexp';
+import { Link } from 'react-router-dom';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
 
@@ -12,6 +12,43 @@ class ListContacts extends Component {
 
   handleChange = (query) =>{
     this.setState({query});
+  }
+
+  clearQuery = () => {
+    this.setState({query:''})
+  }
+
+  getSearchComponent = (query, showingContacts, contacts) => {
+    return (
+      <div className="search row">
+        <div className="col-12 justify-content-center input-group input-group-lg">
+          <input
+            className="form-control col-10 col-lg-6"
+            type="text"
+            placeholder="Search Contact"
+            value={query}
+            onChange={(e) => this.handleChange(e.target.value)}
+            aria-describedby="plusBtn" />
+          <span className="input-group-addon" id="plusBtn">
+            <Link to="/create/contact">
+              <i className="fa fa-user-plus text-primary" aria-hidden="true"></i>
+            </Link>
+          </span>
+        </div>
+        <div className="col-12 justify-content-md-center">
+          {(query) && (
+            <div>
+              <div className="text-center">
+                Now showing <strong>{showingContacts.length}</strong> of <strong>{contacts.length}</strong> total.
+                <br/>
+                <button href='#' className="btn btn-link" onClick={() => this.clearQuery()}>Show all</button>
+              </div>
+            </div>
+            )
+          }
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -31,25 +68,7 @@ class ListContacts extends Component {
 
     return(
       <div>
-        <div className="search row">
-          <div className="col-12 justify-content-center input-group input-group-lg">
-            <input
-              className="form-control col-10 col-lg-6"
-              type="text"
-              placeholder="Search Contact"
-              value={query}
-              onChange={(e) => this.handleChange(e.target.value)}
-              aria-describedby="plusBtn" />
-            <span className="input-group-addon" id="plusBtn">
-              <Link to="/create/contact">
-                <i className="fa fa-user-plus text-primary" aria-hidden="true"></i>
-              </Link>
-            </span>
-          </div>
-          <div className="col-12 justify-content-md-center">
-            <div className="text-center">{query}</div>
-          </div>
-        </div>
+        {this.getSearchComponent(query, showingContacts, contacts)}
 
         <div className="list-contacts-wrapper">
           <ul className="list-contacts row justify-content-center">
