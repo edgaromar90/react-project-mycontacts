a888b0fb9ab61b6cfa25b6fa37961511b0370733
Edgar Henriquez <edgaromar90@gmail.com>
2017-08-28 11:21:51 -0400

    server returns all contacts

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,10 +1,86 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+import platform
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/get/contacts', methods=['GET'])
-def getContacts():
-  return '<h1>We are here</h1>'
+if platform.system() == 'Windows':
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.getcwd()+'\\database.db'
+else:
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getcwd()+'/database.db'
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(30))
+  occupation = db.Column(db.String(20))
+  skills = db.relationship('Skill', backref='contact', lazy='dynamic', cascade='all, delete, delete-orphan')
+
+  def __repr__(self):
+    return "<Contact(name='%s', occupation='%s')>" % (self.name, self.occupation)
+
+class Skill(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  skill_name = db.Column(db.String(20))
+  contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+
+  def __repr__(self):
+    return "<Skill(skill_name='%s')>" % (self.skill_name)
+
+#################### UTILS ################
+
+def formatContact(contact, skills):
+  return {
+    'id':contact.id,
+    'name':contact.name,
+    'occupation':contact.occupation,
+    'skills': skills
+  }
+
+def fetchSkills(contact):
+  '''
+
+  '''
+  skill_list = []
+  try:
+    skills = Skill.query.filter_by(contact=contact)
+    for skill in skills:
+      skill_list.append(skill.skill_name)
+  except:
+    return []
+  else:
+    return skill_list
+
+def fetchContacts():
+  '''
+  Fetch the contacts from the database, it uses "fetchSkills" to gather
+  the skills of those contacts and using "formatContact" to format each
+  contact.
+  @return -> [response] - array of dictionaries [{},{}...]
+  '''
+  response = []
+  try:
+    contacts = Contact.query.all()
+  except:
+    return 'Error with the database'
+
+  if len(contacts) == 0:
+    return 'No contacts found'
+  for contact in contacts:
+    contact_skills = fetchSkills(contact)
+    response.append(formatContact(contact, contact_skills))
+  return response
+
+############### END OF UTILS #####################
+
+@app.route('/get/contact/', methods=['GET'])
+def getAllContacts():
+  contacts = fetchContacts()
+  return jsonify(contacts)
 
 if __name__ == '__main__':
   app.run()
--- server/setup.py
+++ server/setup.py
@@ -2,12 +2,22 @@ from setuptools import setup
 
 setup(
     name='server',
+    author='Enter your name',
+    author_email='enteryour@email.com',
+    description=('My Description'),
     packages=['server'],
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask_sqlalchemy',
+        'flask_cors',
+        'virtualenv'
     ],
 )
-
+#SET FLASK_APP="server" windows
+#SET FLASK_DEBUG="1"
+#$env:FLASK_APP="server" if using powershell
+#$env:FLASK_DEBUG="1"
+#export FLASK_APP=server in Linux
 #pip install -e .
 #flask run
